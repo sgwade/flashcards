45d7c8b6cca75e9fe2f42627c139f5fd330527c2
swade42 <wade.samanthagrace@gmail.com>
2023-08-07 18:37:32 -0700

    add lil api

---
+++ flashcards/src/utils/api.js
@@ -0,0 +1,29 @@
+import {
+    getDecks,
+    getDeck,
+    saveDeckTitle,
+    addCardToDeck,
+} from './data';
+
+const getDecks = function() {
+    return getDecks();
+}
+
+const getDeck = function() {
+    return getDeck(id);
+}
+
+const saveDeckTitle = function(title) {
+    return saveDeckTitle(title);
+}
+
+const addCardToDeck = function(title, card) {
+    return addCardToDeck(title, card);
+}
+
+export {
+    getDecks,
+    getDeck,
+    saveDeckTitle,
+    addCardToDeck,
+}
