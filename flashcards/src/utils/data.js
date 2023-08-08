dc8eb3e18032a99b90f0a5ea458cb8616f6cc523
swade42 <wade.samanthagrace@gmail.com>
2023-08-07 18:35:36 -0700

    dummy api funcs

--- flashcards/src/utils/data.js
+++ flashcards/src/utils/data.js
@@ -1,4 +1,4 @@
-const data = {
+let decks = {
     Sam: {
         title: 'Sam',
         questions: [
@@ -26,3 +26,56 @@ const data = {
         ]
     }
 }
+
+const formatDec = function(title) {
+    return {
+        [title]: {
+            title,
+            questions: [],
+        },
+    };
+}
+
+const getDecks = function() {
+    return new Promise((res, rej) => {
+        setTimeout(() => res({...decks}), 1000);
+    });
+}
+
+const getDeck = function(id) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            decks[id] !== undefined
+            ? res({...decks[id]})
+            : rej(Error("Deck not found!!"));
+        }, 500);
+    });
+}
+
+const saveDeckTitle = function(title) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            decks[title] !== undefined
+            ? rej(Error("That deck name already exists"))
+            : decks[title] = formatDeck(title), res(decks[title]);
+        }, 500);
+    });
+}
+
+const addCardToDeck = function(title, card) {
+    return new Promise((res, rej) => {
+        decks[title] !== undefined
+        ? decks[title] = {
+            ...decks[title],
+            "questions": decks[title].questions.concat([card]),
+        }
+        : rej(Error("Deck not found!!"));
+    })
+}
+
+export {
+    getDecks,
+    getDeck,
+    saveDeckTitle,
+    addCardToDeck,
+};
