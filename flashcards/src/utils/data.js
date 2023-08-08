a2763999c1e467d613fd640b7627770afcade9e3
swade42 <wade.samanthagrace@gmail.com>
2023-08-07 18:27:28 -0700

    add dummy data

---
+++ flashcards/src/utils/data.js
@@ -0,0 +1,28 @@
+const data = {
+    Sam: {
+        title: 'Sam',
+        questions: [
+            {
+                question: 'What is Sam\'s last name',
+                answer: 'Wade'
+            },
+            {
+                question: 'Where does Sam live',
+                answer: 'Long Beach'
+            }
+        ]
+    },
+    Gon: {
+        title: 'Gon',
+        questions: [
+            {
+                question: 'Who is Gon looking for?',
+                answer: 'Ging'
+            },
+            {
+                question: 'Who is Gon\'s best friend?',
+                answer: 'Killua'
+            }
+        ]
+    }
+}
